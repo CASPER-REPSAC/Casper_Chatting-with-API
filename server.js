cafb81b7c08b24c2012dd78fe94af4694a8aeaff
Floodnut <gsniper777@gmail.com>
2021-02-13 17:53:20 +0900

    DB

--- server.js
+++ server.js
@@ -4,31 +4,33 @@ const express = require('express');
 const server = express();
 const session = require('express-session');
 const router = express.Router();
-//const passport = require('passport');
 
-/*  DB require  
-const {chat} = require('./models/chatSch');
-const db = require('./db');
-*/
+/*=====Route Modules=====*/
+const mysql = require('./routes/mysql');
+const routeJoin = require('./routes/join');
+const routeIndex = require('./routes/index');
+const routeSubmit = require('./routes/submit');
 
+
+mysql.connect();
+
+server.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 server.use(express.json()); //json 
 server.set("view engine", "ejs");
 server.use(express.static(__dirname + "/static"));
-
 server.use(session({
     secret: '_C@@5P3RRRC7AT@@@$!zs$dKs24@@aw@$!Op5',
     resave: false,
     saveUninitialized: true
 }));
 
-server.get('/',(req,res)=>{ // "views/index.ejs" is main page
-    res.render('index');
-});
-
-server.get('/join',(req,res)=>{  // "views/register.ejs" is register page"
-    res.render('join');
-});
-
-server.listen("8000",() =>{
+server.get('/',routeIndex);// "views/index.ejs" is main page
+server.get('/join',routeJoin);  // "views/join.ejs" is join page"
+server.post('/submit',routeSubmit);
+server.listen("7000",() =>{
     console.log("Server start...");
 });
